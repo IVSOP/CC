4b24c60e4a7d168059956c71186a30da2975c3ab
Kard9876 <dark9876king@gmail.com>
2023-10-19 21:21:17 +0100

    FS_Track

--- include/common/fs_track.h
+++ include/common/fs_track.h
@@ -3,18 +3,60 @@
 #define TP2_FS_TRACK_H
 
 #include <cstdint>
+#include <netinet/in.h>
+#include <vector>
+#include <string>
+
+#define FILENAME_SIZE 256
 
 class FS_Track{
 private:
 
 public:
-    struct RegData{
+    FS_Track();
+    ~FS_Track();
 
+    struct RegData{
+        char filename[FILENAME_SIZE]{};
+
+        RegData(char*);
+        ~RegData();
     };
 
-    uint8_t opcode_opts;
-    uint8_t size[3];
-    uint64_t id;
+    struct IDAssignmentData{
+        char filename[FILENAME_SIZE];
+        uint64_t file_id;
+
+        IDAssignmentData(char*, uint64_t);
+        ~IDAssignmentData();
+    };
+
+    struct UpdateFileBlocksData{
+        uint64_t file_id;
+        uint32_t block_number;
+
+        UpdateFileBlocksData(uint64_t, uint32_t);
+        ~UpdateFileBlocksData();
+    };
+
+    struct GetFileBlocksData{
+        struct in_addr ip;
+        std::vector<uint32_t> block_numbers;
+
+        GetFileBlocksData(struct in_addr, std::vector<uint32_t>);
+        ~GetFileBlocksData();
+    };
+
+    struct ErrorMessageData{
+        std::string details;
+
+        ErrorMessageData(std::string);
+        ~ErrorMessageData();
+    };
+
+    uint8_t opcode_opts{};
+    uint8_t size[3]{};
+    uint64_t id{};
 
     uint8_t fs_track_getOpcode();
 
@@ -24,10 +66,20 @@ public:
 
     uint64_t fs_track_getId();
 
-    void Reg_send_data();
+    void Reg_send_data(char* filename);
     RegData Reg_read_data();
 
+    void IDAssignment_send_data(char* filename, uint64_t id);
+    IDAssignmentData IDAssignment_read_data();
 
+    void UpdateFileBlocks_send_data(uint64_t id, uint32_t block_number);
+    UpdateFileBlocksData UpdateFileBlocks_read_data();
+
+    void GetFileBlocks_send_data(struct in_addr ip, std::vector<uint32_t>& block_numbers);
+    GetFileBlocksData GetFileBlocks_read_data();
+
+    void ErrorMessage_send_data(std::string& details);
+    ErrorMessageData ErrorMessage_read_data();
 };
 
 #endif //TP2_FS_TRACK_H
--- src/common/fs_track.cpp
+++ src/common/fs_track.cpp
@@ -1,4 +1,61 @@
+#include <cstring>
 #include "fs_track.h"
+
+/* Constructors */
+
+FS_Track::FS_Track() {
+    this->opcode_opts;
+    this->size;
+    this->id;
+}
+
+FS_Track::~FS_Track() = default;
+
+FS_Track::RegData::RegData(char* filename) {
+    int len = strlen(filename);
+
+    memcpy(this->filename, filename, len);
+
+    this->filename[len] = '\0';
+}
+
+FS_Track::RegData::~RegData() = default;
+
+FS_Track::IDAssignmentData::IDAssignmentData(char* filename, uint64_t file_id) {
+    int len = strlen(filename);
+
+    memcpy(this->filename, filename, len);
+
+    this->filename[len] = '\0';
+
+    this->file_id = file_id;
+}
+
+FS_Track::IDAssignmentData::~IDAssignmentData() = default;
+
+FS_Track::UpdateFileBlocksData::UpdateFileBlocksData(uint64_t file_id, uint32_t block_number) {
+    this->file_id = file_id;
+    this->block_number = block_number;
+}
+
+FS_Track::UpdateFileBlocksData::~UpdateFileBlocksData() = default;
+
+FS_Track::GetFileBlocksData::GetFileBlocksData(struct in_addr ip, std::vector<uint32_t> block_numbers) {
+    this->ip = ip;
+    this->block_numbers = std::vector(block_numbers);
+}
+
+FS_Track::GetFileBlocksData::~GetFileBlocksData() = default;
+
+FS_Track::ErrorMessageData::ErrorMessageData(std::string details) {
+    this->details = std::string();
+
+    this->details.assign(details);
+}
+
+FS_Track::ErrorMessageData::~ErrorMessageData() = default;
+
+/* Constructors end */
 
 uint8_t FS_Track::fs_track_getOpcode() {
     uint8_t opcode = this->opcode_opts;
@@ -20,4 +77,77 @@ uint32_t FS_Track::fs_track_getSize() {
 }
 uint64_t FS_Track::fs_track_getId() {
     return this->id;
+}
+
+void FS_Track::Reg_send_data(char* filename) {
+    FS_Track::RegData data = FS_Track::RegData(filename);
+
+    // TODO Send data trough socket
+}
+
+FS_Track::RegData FS_Track::Reg_read_data() {
+    // TODO from socket, read data to struct
+
+    char* filename;
+
+    return FS_Track::RegData(filename);
+}
+
+void FS_Track::IDAssignment_send_data(char* filename, uint64_t file_id) {
+    FS_Track::IDAssignmentData data = FS_Track::IDAssignmentData(filename, file_id);
+
+    // TODO Send data trough socket
+}
+
+FS_Track::IDAssignmentData FS_Track::IDAssignment_read_data() {
+    // TODO from socket, read data to struct
+
+    char* filename;
+    uint64_t file_id;
+
+    return FS_Track::IDAssignmentData(filename, file_id);
+}
+
+void FS_Track::UpdateFileBlocks_send_data(uint64_t file_id, uint32_t block_number) {
+    FS_Track::UpdateFileBlocksData data = FS_Track::UpdateFileBlocksData(file_id, block_number);
+
+    // TODO Send data trough socket
+}
+
+FS_Track::UpdateFileBlocksData FS_Track::UpdateFileBlocks_read_data() {
+    // TODO from socket, read data to struct
+
+    uint64_t file_id;
+    uint32_t block_number;
+
+    return FS_Track::UpdateFileBlocksData(file_id, block_number);
+}
+
+void FS_Track::GetFileBlocks_send_data(struct in_addr ip, std::vector<uint32_t>& block_numbers) {
+    FS_Track::GetFileBlocksData data = FS_Track::GetFileBlocksData(ip, block_numbers);
+
+    // TODO Send data trough socket
+}
+
+FS_Track::GetFileBlocksData FS_Track::GetFileBlocks_read_data() {
+    // TODO from socket, read data to struct
+
+    struct in_addr ip;
+    std::vector<uint32_t> block_numbers;
+
+    return FS_Track::GetFileBlocksData(ip, block_numbers);
+}
+
+void FS_Track::ErrorMessage_send_data(std::string& details) {
+    FS_Track::ErrorMessageData data = FS_Track::ErrorMessageData(details);
+
+    // TODO Send data trough socket
+}
+
+FS_Track::ErrorMessageData FS_Track::ErrorMessage_read_data() {
+    // TODO from socket, read data to struct
+
+    std::string details = std::string();
+
+    return FS_Track::ErrorMessageData(details);
 }
