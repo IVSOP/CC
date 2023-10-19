b225bf5a263c8b09d2b75d8fcbc57d16ed826a92
Kard9876 <dark9876king@gmail.com>
2023-10-19 17:59:04 +0100

    Includes

---
+++ include/common/fs_track.h
@@ -0,0 +1,8 @@
+//
+// Created by mafba on 19/10/2023.
+//
+
+#ifndef TP2_FS_TRACK_H
+#define TP2_FS_TRACK_H
+
+#endif //TP2_FS_TRACK_H
---
+++ include/server/main.h
@@ -0,0 +1,8 @@
+//
+// Created by mafba on 19/10/2023.
+//
+
+#ifndef TP2_MAIN_H
+#define TP2_MAIN_H
+
+#endif //TP2_MAIN_H
