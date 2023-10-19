1a58c108f62bdc3e354dba47cb1c9aae510d48b2
Kard9876 <dark9876king@gmail.com>
2023-10-19 18:39:21 +0100

    CMake

---
+++ include/common/read_header.h
@@ -0,0 +1,8 @@
+//
+// Created by mafba on 19/10/2023.
+//
+
+#ifndef TP2_READHEADER_H
+#define TP2_READHEADER_H
+
+#endif //TP2_READHEADER_H
---
+++ src/common/read_header.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mafba on 19/10/2023.
+//
+
+#include "read_header.h"
