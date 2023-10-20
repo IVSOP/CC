46014eacbdaf1ad06d3f4dd1e5fac8e134c377b5
Ivan Ribeiro <ivan.ribeiro09s@gmail.com>
2023-10-20 23:59:53 +0100

    Add FS_Transfer class

---
+++ debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="debug"
+
+if cmake -DCMAKE_BUILD_TYPE=Debug -S . -B $DIR; then
+	printf "\n"
+	if make -j$(nproc) -C $DIR; then
+		printf "\n"
+	else
+		printf ">> build failed\n"
+	fi
+else
+	printf ">> configure failed\n"
+fi
--- include/common/fs_transfer.h
+++ include/common/fs_transfer.h
@@ -7,21 +7,60 @@
 
 
 #include <cstdint>
+#include <stddef.h>
 
-class FS_Transfer{
-private:
+#define FS_TRANSFER_DATA_SIZE 1024 // uint32 + 1 kib de dados
 
-public:
+struct BlockData {
+	uint32_t blockID;
+	char data[FS_TRANSFER_DATA_SIZE];
+};
+
+struct FS_Transfer {
+	FS_Transfer() = default;
+	~FS_Transfer() = default;
 
     uint32_t checksum;
     uint32_t opc_size;
     uint64_t id;
+	// void * data;
+	BlockData data;
 
-    uint32_t fs_track_getChecksum();
-    uint8_t fs_track_getOpcode();
-    uint32_t fs_track_getSize();
-    uint64_t fs_track_getId();
-    
+    constexpr uint32_t getChecksum() const {
+		return checksum;
+	}
+
+    constexpr uint8_t getOpcode() const {
+		uint8_t fstByte = static_cast<uint8_t> (this->opc_size);
+		fstByte = (fstByte & 0xC0) >> 6;
+		return fstByte;
+	}
+
+    constexpr uint32_t getSize() const {
+		uint32_t size = this->opc_size;
+		size = (size & 0x3F);
+		return size;
+	}
+
+    constexpr uint64_t getId() const {
+		return id;
+	}
+
+	constexpr void *getData() {
+		return static_cast<void *>(&data);
+	}
 };
+
+	#define FS_TRANSFER_DEBUG
+	#ifdef FS_TRANSFER_DEBUG
+
+		static_assert(offsetof(FS_Transfer, data) == sizeof(uint32_t) + sizeof(uint32_t) + sizeof(uint64_t), "Error: FS_Transfer has padding which is not accounted for");
+		// isto nao da pq??????????????????? static_assert(offsetof(BlockData, data) == sizeof(uint32_t), "Error: BlockData has padding which is not accounted for");
+		static_assert(sizeof(BlockData) == sizeof(char) * 1024 + sizeof(uint32_t), "Error: BlockData has padding which is not accounted for");
+
+	#undef FS_TRANSFER_DEBUG // ?????
+
+	#endif
+
 
 #endif //TP2_FS_TRANSFER_CPP_H
