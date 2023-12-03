ed962f708e25eb4f4188ee05e2eadbd0e672d1a3
Kard9876 <dark9876king@gmail.com>
2023-12-03 23:38:11 +0000

    Test

--- src/server/server_main.cpp
+++ src/server/server_main.cpp
@@ -14,9 +14,10 @@
 #include "server.h"
 #include "errors.h"
 
-#define BUFFER_SIZE (uint32_t) 1500
+#define BUFFER_SIZE (uint32_t)1500
 
-void read_data(Server& server, ServerTCPSocket::SocketInfo& connection, FS_Track message) {
+void read_data(Server &server, ServerTCPSocket::SocketInfo &connection, FS_Track message)
+{
     uint8_t OPCode = message.fsTrackGetOpcode();
     std::string errorDetails;
     std::vector<FS_Track::PostFileBlocksData> data;
@@ -24,74 +25,76 @@ void read_data(Server& server, ServerTCPSocket::SocketInfo& connection, FS_Track
     char hostname[LEN_HOSTNAME];
     int status;
 
+    switch (OPCode)
+    {
+    // Cases 0 and 1 are the same
+    // Register node
+    case 0:
 
+    // Update node
+    case 1:
+        status = getnameinfo((struct sockaddr *)&connection.addr, sizeof(struct sockaddr_in), hostname, LEN_HOSTNAME, NULL, 0, 0);
+        if (status != 0)
+        {
+            print_error("There has been an error aquiring the host name");
+        }
 
-    switch (OPCode) {
-        // Cases 0 and 1 are the same
-        // Register node
-        case 0:
+        node = std::string(hostname);
 
-        // Update node
-        case 1:
-            status = getnameinfo((struct sockaddr *)&connection.addr, sizeof(struct sockaddr_in), hostname, LEN_HOSTNAME, NULL, 0, 0);
-            if(status != 0){
-                print_error("There has been an error aquiring the host name");
-            }
+        printf("Received register/update message from node %s\n", hostname);
 
-            node = std::string(hostname);
+        server.registerUpdateNode(node, message.regUpdateDataGetData());
+        break;
 
-            printf("Received register/update message from node %s\n", hostname);
+    // Get Message
+    case 2:
 
-            server.registerUpdateNode(node, message.regUpdateDataGetData());
-            break;
+        printf("Received get message from node %s\n", inet_ntoa(connection.addr));
+        data = server.getNodesWithFile(message.fsTrackGetHash());
+        FS_Track::sendPostMessage(connection, message.fsTrackGetHash(), data);
+        break;
 
-        // Get Message
-        case 2:
+    // Post Message
+    case 3:
+        errorDetails = "Only a server can send a Post Message";
+        FS_Track::sendErrorMessage(connection, errorDetails);
+        break;
 
-            printf("Received get message from node %s\n", inet_ntoa(connection.addr));
-            data = server.getNodesWithFile(message.fsTrackGetHash());
-            FS_Track::sendPostMessage(connection, message.fsTrackGetHash(), data);
-			break;
+    // Error Message
+    case 4:
+        // TODO what to do with error messages
+        break;
 
-        // Post Message
-        case 3:
-            errorDetails = "Only a server can send a Post Message";
-            FS_Track::sendErrorMessage(connection, errorDetails);
-            break;
+    // ByeBye Message
+    case 5:
+        status = getnameinfo((struct sockaddr *)&connection.addr, sizeof(struct sockaddr_in), hostname, LEN_HOSTNAME, NULL, 0, 0);
+        if (status != 0)
+        {
+            print_error("There has been an error aquiring the host name");
+        }
 
-        // Error Message
-        case 4:
-            // TODO what to do with error messages
-            break;
+        node = std::string(hostname);
 
-        // ByeBye Message
-        case 5:
-            status = getnameinfo((struct sockaddr *)&connection.addr, sizeof(struct sockaddr_in), hostname, LEN_HOSTNAME, NULL, 0, 0);
-            if(status != 0){
-                print_error("There has been an error aquiring the host name");
-            }
+        printf("Received bye bye message from node %s\n", hostname);
 
-            node = std::string(hostname);
-
-            printf("Received bye bye message from node %s\n", hostname);
-
-            server.deleteNode(node);
-            break;
-        default:
-            errorDetails = "Message has invalid OPCode";
-            FS_Track::sendErrorMessage(connection, errorDetails);
-            break;
+        server.deleteNode(node);
+        break;
+    default:
+        errorDetails = "Message has invalid OPCode";
+        FS_Track::sendErrorMessage(connection, errorDetails);
+        break;
     }
 }
 
-
-void serveClient(ServerTCPSocket::SocketInfo connection, Server& serverData, std::mutex& mtx, std::condition_variable& cdt) {
+void serveClient(ServerTCPSocket::SocketInfo connection, Server &serverData, std::mutex &mtx, std::condition_variable &cdt)
+{
     FS_Track message = FS_Track();
-    uint8_t* buffer = new uint8_t[BUFFER_SIZE];
+    uint8_t *buffer = new uint8_t[BUFFER_SIZE];
 
     puts("Connection initiated.");
 
-    while (FS_Track::readMessage(message, buffer, BUFFER_SIZE, connection)) {
+    while (FS_Track::readMessage(message, buffer, BUFFER_SIZE, connection))
+    {
         read_data(serverData, connection, message);
         message = FS_Track();
     }
@@ -101,42 +104,49 @@ void serveClient(ServerTCPSocket::SocketInfo connection, Server& serverData, std
     message = FS_Track(5);
     read_data(serverData, connection, message);
 
-    delete[] (uint8_t*) buffer;
+    delete[] (uint8_t *)buffer;
 
     std::unique_lock<std::mutex> lock = std::unique_lock<std::mutex>(mtx);
     cdt.notify_one();
 }
 
-void acceptClients(Server& serverData, std::vector<ThreadRAII>& threadGraveyard, std::mutex& mtx, std::condition_variable& cdt) {
+void acceptClients(Server &serverData, std::vector<ThreadRAII> &threadGraveyard, std::mutex &mtx, std::condition_variable &cdt)
+{
     ServerTCPSocket serverSocket = ServerTCPSocket();
     serverSocket.socketListen();
 
     ServerTCPSocket::SocketInfo new_connection;
 
-    while (true) {
+    while (true)
+    {
         new_connection = serverSocket.acceptClient();
 
         threadGraveyard.emplace_back(std::thread(serveClient, new_connection, std::ref(serverData), std::ref(mtx), std::ref(cdt)), ThreadRAII::DtorAction::join);
     }
 }
 
-void cleanUpThreads(std::vector<ThreadRAII>& threadGraveyard, std::mutex& mtx, std::condition_variable& cdt){
+void cleanUpThreads(std::vector<ThreadRAII> &threadGraveyard, std::mutex &mtx, std::condition_variable &cdt)
+{
     std::unique_lock<std::mutex> lock = std::unique_lock<std::mutex>(mtx);
-    while(true){
+    while (true)
+    {
         cdt.wait(lock);
 
         threadGraveyard.erase(threadGraveyard.begin(), threadGraveyard.end());
     }
 }
 
-int main() {
+int main()
+{
     Server server = Server();
 
     std::mutex mtx = std::mutex();
     std::condition_variable cdt = std::condition_variable();
 
     std::vector<ThreadRAII> threadGraveyard = std::vector<ThreadRAII>();
-    ThreadRAII cleanUp(std::thread([&](){ cleanUpThreads(threadGraveyard, mtx, cdt); /*Lambda Function*/}), ThreadRAII::DtorAction::join);
+    ThreadRAII cleanUp(std::thread([&]()
+                                   { cleanUpThreads(threadGraveyard, mtx, cdt); /*Lambda Function*/ }),
+                       ThreadRAII::DtorAction::join);
 
     acceptClients(server, threadGraveyard, mtx, cdt);
 }
